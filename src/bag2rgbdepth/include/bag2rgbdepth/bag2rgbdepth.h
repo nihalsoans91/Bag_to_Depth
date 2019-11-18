3e242e5055889df737414800429cdcb5e31c67f9
NihalSoans91 <nihalsoans91@gmail.com>
2019-11-17 21:56:17 -0500

    Added multi threaded call for depth and RGB

--- src/bag2rgbdepth/include/bag2rgbdepth/bag2rgbdepth.h
+++ src/bag2rgbdepth/include/bag2rgbdepth/bag2rgbdepth.h
@@ -2,5 +2,13 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
 
+void RGBCallback(const sensor_msgs::ImageConstPtr& msg);
+void DepthCallback(const sensor_msgs::ImageConstPtr& msg);
 
+bool firstRGB = false;
+bool firstDepth = false;
+uint64_t rgbImages = 0;
+uint64_t depthImages = 0;
--- src/bag2rgbdepth/src/bag2rgbdepth.cpp
+++ src/bag2rgbdepth/src/bag2rgbdepth.cpp
@@ -1,10 +1,37 @@
 # include <bag2rgbdepth.h>
 
 void RGBCallback(const sensor_msgs::ImageConstPtr& msg){
-    ROS_INFO("RGB");
+    cv::Mat image;
+    cv_bridge::CvImagePtr cv_ptr;
+    if ( firstDepth )
+    {
+        try{
+            cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
+        }
+        catch (cv_bridge::Exception& e){
+            ROS_ERROR("cv_bridge exception RGB: %s", e.what());
+            return ;
+        }
+    }
+    std::string filename = "rgb_" + std::to_string(rgbImages) + ".jpg" ;
+    cv::imwrite("rgb.jpg",cv_ptr->image);
+    firstRGB = true ;
 }
 void DepthCallback(const sensor_msgs::ImageConstPtr& msg){
-    ROS_INFO("Depth");
+    cv::Mat image;
+    cv_bridge::CvImagePtr cv_ptr;
+    if ( firstRGB ){
+        try{
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_16UC1);
+        }
+        catch (cv_bridge::Exception& e){
+            ROS_ERROR("cv_bridge exception DEPTH: %s", e.what());
+            return ;
+        }
+    }
+    std::string filename = "depth_" + std::to_string(depthImages) + ".jpg";
+    // cv::imwrite("septh.jpg" ,cv_ptr->image);
+    firstDepth = true;
 }
 
 int main(int argc, char** argv)
@@ -13,9 +40,9 @@ int main(int argc, char** argv)
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
     ROS_INFO("Running");
-    it.subscribe("/camera/rgb/image_raw", 1, RGBCallback);
-    // it.subscribeCamera("/camera/rgb/image_raw",1, boost::bind(&RGBCallback, _1));
-    // it.subscribeCamera("/camera/depth_registered/image_raw",1, boost::bind(&DepthCallback, _1));
+    // it.subscribe("/camera/rgb/image_raw", 1, RGBCallback);
+    image_transport::CameraSubscriber subrgb = it.subscribeCamera("/camera/rgb/image_raw",1, boost::bind(&RGBCallback, _1));
+    image_transport::CameraSubscriber subdepth = it.subscribeCamera("/camera/depth_registered/image_raw",1, boost::bind(&DepthCallback, _1));
     ROS_INFO("Spinning");
     ros::spin();
 
